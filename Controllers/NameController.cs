0d9a4fbbf1ab104327680226a952fbe3394efd79
kshitijchaudhary <chaudharykshitij49@gmail.com>
2024-03-18 21:06:34 -0400

    first commit

---
+++ Controllers/NameController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32.SafeHandles;
+
+namespace InventoryManagementSystem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+       
+
+        public NameController()
+        {
+            // dbobject = orm db 
+        }
+
+        [HttpGet(Name = "GetName")]
+        public String Get()
+        {
+            return "Hello  World";
+
+            //db.Customers.returnAlla());
+        }
+    }
+}
